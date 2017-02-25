0ea2bd4f98806dc0b878437fe8acdc0432c1f6a7
boatengfrankenstein <boatengfrankenstein@gmail.com>
2017-02-25 08:19:14 -0500

    Update

---
+++ app/src/main/java/com/microhealthllc/mbmicalc/EnterBMIINfo.java
@@ -0,0 +1,13 @@
+package com.microhealthllc.mbmicalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterBMIINfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_bmiinfo);
+    }
+}
